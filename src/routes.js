c3c91a1d950e8c04a5c213f6a0ca865050abc51e
Otavio Marques <marquesotavio251204@gmail.com>
2023-02-06 14:45:23 -0300

    adicionando eslint

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,12 @@
 import { Router } from 'express';
-import custumers from './app/controllers/CustumersController'
+import custumers from './app/controllers/CustumersController';
 
-const routes = new Router()
+const routes = new Router();
 
-console.log("teste");
+routes.get('/customers', custumers.index);
+routes.get('/customers/:id', custumers.show);
+routes.post('/customers', custumers.create);
+routes.put('/customers/:id', custumers.update);
+routes.delete('/customers:id', custumers.destroy);
 
-
-routes.get('/customers', custumers.index)
-routes.get('/customers/:id', custumers.show)
-routes.post('/customers', custumers.create)
-routes.put('/customers/:id', custumers.update)
-routes.delete('/customers:id', custumers.destroy)
-
-
-module.exports = routes
+module.exports = routes;
