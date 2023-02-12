4d40f1c63c60d167db056483a3219ad56c5f5cb1
Otavio Marques <marquesotavio251204@gmail.com>
2023-02-11 21:02:23 -0300

    commite para o paiton

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports ={
+	dialect: 'postgres',
+	host: 'localhost',
+	username: 'postgres',
+	password: '123',
+	database: 'postgres',
+	port: 5432,
+	define: {
+		timestamp: true,
+		underscored: true,
+	}
+
+};
